dd4f5ad38444aeb61d1e8a411ccfdac332e3c420
rassi0429 <taitokokoa+rassi@gmail.com>
2023-09-28 15:10:21 +0900

    fix: 早期リターンの修正

--- main.mjs
+++ main.mjs
@@ -9,7 +9,7 @@ client.on('ready', () => {
 });
 
 client.on("messageCreate", async (msg) => {
-    if(!msg.flags.has(1) && !msg.content.startsWith("!")) return
+    if(!msg.flags.has(1)) return
 
     const t = await translate(msg.content)
     await msg.channel.send(t)
