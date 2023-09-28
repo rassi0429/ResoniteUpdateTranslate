fdb23160cfdf6acb4c4279ac01c3715f2cc5d27a
rassi0429 <taitokokoa+rassi@gmail.com>
2023-09-28 10:01:59 +0900

    feat: init release

---
+++ main.mjs
@@ -0,0 +1,37 @@
+import { Client, GatewayIntentBits  } from 'discord.js';
+const client = new Client({ intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent] });
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on("messageCreate", async (msg) => {
+    if(!msg.flags.has(1) && !msg.content.startsWith("!")) return
+
+    const t = await translate(msg.content)
+    await msg.channel.send(t)
+    await sendWebHook(process.env.WEBHOOKS, t)
+  });
+
+
+client.login(process.env.D_TOKEN);
+
+async function translate(text) {
+    const res = await fetch("https://translate-api.kokoa.dev/v1/translate?from=en&to=ja&text=" + text)
+    const t = await(res.text())
+    return t
+}
+
+async function sendWebHook(url, text) {
+    const res = await fetch(url, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({"content": text})
+    })
+    return res
+}
