64e971c0504c60f5cd014dee892badcf127977fc
rassi0429 <taitokokoa+rassi@gmail.com>
2023-10-31 15:33:18 +0900

    feat: chatgpt translate

--- main.mjs
+++ main.mjs
@@ -1,9 +1,15 @@
 import { Client, GatewayIntentBits } from 'discord.js';
+import openai, { Configuration, OpenAIApi } from "openai"
+
 const client = new Client({
     intents: [
         GatewayIntentBits.Guilds,
         GatewayIntentBits.GuildMessages,
         GatewayIntentBits.MessageContent]
+});
+
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
 });
 
 client.on('ready', () => {
@@ -17,8 +23,6 @@ client.on("messageCreate", async (msg) => {
     await msg.channel.send(t)
 
     console.log(msg)
-    // get crosspost from server name
-
 
     await sendWebHook(process.env.WEBHOOKS, t, msg.author.username, msg.author.avatarURL() ?? null)
 });
@@ -27,9 +31,17 @@ client.on("messageCreate", async (msg) => {
 client.login(process.env.D_TOKEN);
 
 async function translate(text) {
-    const res = await fetch("https://translate-api.kokoa.dev/v1/translate?from=en&to=ja&text=" + text)
-    const t = await (res.text())
-    return t
+    const completion = await openai.createChatCompletion({
+        model: "gpt-4",
+        messages: [
+            { "role": "system", "content": "以下のリリースノートを日本語に翻訳してください。　返答は翻訳内容だけで大丈夫です。 issueはissueのままで大丈夫です。" },
+            { "role": "user", "content": text }
+        ],
+    });
+
+    const reply = completion.data.choices[0].message.content;
+    console.log(reply.slice(0, 10), completion.data.usage.total_tokens)
+    return reply
 }
 
 async function sendWebHook(url, text, channelName, avatarURL) {
